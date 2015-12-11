4b4f13ecc73cbcc27114c968e0091383531bb83a
Davide Corio <davide.corio@abstract.it>
2015-12-11 15:10:12 +0100

    [ADD] purchase_prder_sequence_fy POC

---
+++ purchase_order_sequence_fy/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015 davide.corio@abstract.it
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Order Sequence FY",
+    "summary": "Fiscalyear support for PO sequence",
+    "version": "8.0.1.0.0",
+    "category": "Purchase Management",
+    "website": "https://odoo-community.org/",
+    "author": "Abstract, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale",
+        "account_auto_sequence_fy"
+    ],
+}
