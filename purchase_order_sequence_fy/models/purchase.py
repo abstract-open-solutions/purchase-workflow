45c7a855af32b642429c6e7e55fe553e98537dc0
Simone Orsi <simahawk@gmail.com>
2015-12-28 10:32:13 +0100

    [fix] be defensive in case we do not have `date_order` into vals

--- purchase_order_sequence_fy/models/purchase.py
+++ purchase_order_sequence_fy/models/purchase.py
@@ -11,6 +11,7 @@ class PurchaseOrder(models.Model):
     def create(self, cr, uid, vals, context=None):
         context = context or {}
         fy_model = self.pool['account.fiscalyear']
-        fy_id = fy_model.find(cr, uid, vals['date_order'])
-        context.update({'fiscalyear_id': fy_id})
+        if 'date_order' in vals:
+            fy_id = fy_model.find(cr, uid, vals['date_order'])
+            context.update({'fiscalyear_id': fy_id})
         return super(PurchaseOrder, self).create(cr, uid, vals, context)
